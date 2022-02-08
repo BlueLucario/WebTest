e3dd113558405eb2e385b9ac756e5a8202146f71
BlueLucario <Benjaminweeg@gmail.com>
2022-02-08 13:46:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://github.com/mrdoob/three.js/blob/680f6c9e72c9635e952e36d6534f0c6689761a9d/build/three.min.js';
-import { OrbitControls } from '../../libs/three125/OrbitControls.js';
+import { OrbitControls } from 'https://github.com/mrdoob/three.js/blob/680f6c9e72c9635e952e36d6534f0c6689761a9d/examples/jsm/controls/OrbitControls.js';
 
 class App{
 	constructor(){
